127253ab7cf5e9516df5c3aefda666b4701352ca
Majd.Kh.Sh <mjdshbyr449@gmail.com>
2026-09-14 18:49:36 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='amin-building-v42';
-const CORE=['./','./index.html','./app-v31.js?v=42.0','./styles-v31.css?v=42.0','./initial-data.js','./firebase-config.js','./firebase-init-v31.js','./manifest.webmanifest?v=42.0','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='amin-building-v44';
+const CORE=['./','./index.html','./app-v31.js?v=44.0','./styles-v31.css?v=44.0','./initial-data.js','./firebase-config.js','./firebase-init-v31.js','./manifest.webmanifest?v=44.0','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new URL(e.request.url);if(u.origin===location.origin){e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,copy));return r;}).catch(()=>caches.match('./index.html'))));}});
